64bc70814e32cbff9fcdca9c0a87d76ee4ed0e08
ukibbb <lucas.bulinski@gmail.com>
2022-12-24 15:10:58 +0100

    af is bettter

--- init.lua
+++ init.lua
@@ -280,7 +280,7 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 -- [[ Harpoon ]]
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
-vim.keymap.set('n', '<leader>ah', mark.add_file)
+vim.keymap.set('n', '<leader>af', mark.add_file)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<C-t>', function() ui.nav_file(2) end)
@@ -552,4 +552,4 @@ require("toggleterm").setup {
   open_mapping = [[<c-\>]]
 }
 
-require("nvim-autopairs").setup {} 
+require("nvim-autopairs").setup {}
