9e8cd2b0f6641520bf31b13d850c8117f2d462b1
ukibbb <lucas.bulinski@gmail.com>
2022-12-25 13:13:57 +0100

    option changes

--- init.lua
+++ init.lua
@@ -85,37 +85,49 @@ require('packer').startup(function(use)
 
 end)
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- Show relative number
+vim.o.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+vim.o.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.o.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.o.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.o.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.o.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.o.pumheight = 10                          -- pop up menu height
+vim.o.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
+vim.o.showtabline = 2                         -- always show tabs
+vim.o.smartcase = true                        -- smart case
+vim.o.smartindent = true                      -- make indenting smarter again
+vim.o.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.o.splitright = true                       -- force all vertical splits to go to the right of current window
+vim.o.swapfile = false                        -- creates a swapfile
+vim.o.timeoutlen = 300                        -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.undofile = true                         -- enable persistent undo
+vim.o.updatetime = 300                        -- faster completion (4000ms default)
+vim.o.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.o.expandtab = true                        -- convert tabs to spaces
+vim.o.shiftwidth = 2                          -- the number of spaces inserted for each indentation
+vim.o.tabstop = 2                             -- insert 2 spaces for a tab
+vim.o.cursorline = true                       -- highlight the current line
+vim.o.number = true                           -- set numbered lines
+vim.o.numberwidth = 4                         -- set number column width to 2 {default 4}
+vim.o.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+vim.o.wrap = true                             -- display lines as one long line
+vim.o.linebreak = true                        -- companion to wrap, don't split words
+vim.o.scrolloff = 8                           -- minimal number of screen lines to keep above and below the cursor
+vim.o.sidescrolloff = 8                       -- minimal number of screen columns either side of cursor if wrap is `false`
+
 vim.o.relativenumber = true
 vim.o.nu = true
--- Set highlight on search
-vim.o.hlsearch = false
 vim.o.incsearch = true
--- Make line numbers default
-vim.wo.number = true
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
 
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
 -- Set colorscheme
 vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
 
-vim.o.smartindent = true
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-
-vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
@@ -197,6 +209,11 @@ vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv" )
 
 -- Nvimtree 
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<cr>')
+
+-- Cancelation
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("v", "<C-c>", "<Esc>")
+
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
